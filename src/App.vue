039bf51c17eee040d50145249efefdb181462d1d
Riccardo-Mauri <163997807+Riccardo-Mauri@users.noreply.github.com>
2024-09-12 13:12:04 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+//1- Importazione del componenente necessario che voglio aggiungere qua
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  //2- Dichiarazione del componente 
+  components: {
+    AppHeader
+  },
+  methods: {
+    incrementCount(){
+      this.count++;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <!--3- Utilizzo del componenete -->
+    <AppHeader />
+   <button @click="incrementCount()">
+    {{ count }}
+   </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Template Vite + Vue'
+    }
+  },
+  
+}
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>
+        {{ message }}
+      </h1>
+
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header{
+  background-color: aquamarine;
+  text-align: center;
+  padding: 25px 0;
+}
+h1{
+  color: lightcoral;
+}
+</style>
